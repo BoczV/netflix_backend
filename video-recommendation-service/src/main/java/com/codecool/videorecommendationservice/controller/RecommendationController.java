2a9ef0e69708d8673936cfa22d486574f6bf9c3c
Viktor Bocz <beviktor95@gmail.com>
2020-09-19 21:50:21 +0200

    Shell of Recommendation service controller

---
+++ video-recommendation-service/src/main/java/com/codecool/videorecommendationservice/controller/RecommendationController.java
@@ -0,0 +1,29 @@
+package com.codecool.videorecommendationservice.controller;
+
+import com.codecool.videorecommendationservice.RecommendationRepository;
+import com.codecool.videorecommendationservice.model.Recommendation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/recommendations")
+public class RecommendationController {
+
+    @Autowired
+    RecommendationRepository recommendationRepository;
+
+    @GetMapping("/{videoId}/all")
+    public List<Recommendation> getRecommendations(@PathVariable("videoId") Long videoId){
+        List<Recommendation> allByVideoId = recommendationRepository.findAllByVideoId(videoId);
+        return allByVideoId;
+    }
+
+    @PostMapping("/add")
+    public void addNewRecommendation(@RequestBody Map<String, Object> body){
+
+    }
+
+}
